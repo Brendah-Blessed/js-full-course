34037fcfec360f25a691fdd53aa1de6c1250f6c7
BrendahBlessed <wawerubrendah63@gmail.com>
2024-06-25 15:16:37 +0300

    finished operators

---
+++ Operators/index.js
@@ -0,0 +1,37 @@
+//operators
+//arithmentic operations 
+/**
+ * addition (+)
+ * subtraction (-)
+ * division (/)
+ * multiplication (*)
+ * modulus (%): returns the remainder e.g 10%3 = 1;
+ */
+//modulus example
+let a = 11;
+let b = 3;
+let c = a%b;
+console.log(c)
+//comparison operators: used to check equality
+/**
+ * > greater than
+ * < less than
+ * >= greater or equal to 
+ * <= less or equal to
+ * == equal to
+ * != not equal to
+ * === strict equal to
+ * !== not equal to  strict
+ */
+let age = 21;
+let myAge = 20;
+
+console.log(age!==myAge)
+
+//Logical operators
+/**
+ * AND && both condtions must be true
+ * OR || atleast one condition must be true
+ * NOT !
+ */
+
