7c2d38af17db80d2308899cdd6b0956c6fd40773
BrendahBlessed <wawerubrendah63@gmail.com>
2024-06-26 14:38:14 +0300

    practicals

---
+++ Practice/index.js
@@ -0,0 +1,30 @@
+//create a javascript program that prompts a user 
+/**a  user to enter 3 numbers and displays the largest of the three
+ * should also display a message if the numbers are equal
+ * should also display a message if the user enter input that is not a number
+ * 
+ * hint use logical operator AND, comparison operators eg greater than
+ * and if statements
+ */
+
+ const firstName = window.prompt("Enter your name")  
+ const secondName = window.prompt("Enter your name")  
+ const thirdName = window.prompt("Enter you name") 
+
+ let a = Number(firstName)
+ let b = Number(secondName)
+ let c = Number(thirdName)
+
+ if(a>b && a>c){
+    alert(" A  is greater")
+ }else if(b>c && b>a ){
+    alert(" B is greater")
+ }else if(c>a && c>b){
+    alert("C is greater")
+ }else if(b===c && b===a ){
+    alert("It is equal")
+ }else{
+    alert("Not Valid")
+ }
+
+    
--- conditional statements/index.js
+++ conditional statements/index.js
@@ -5,10 +5,10 @@
 
 
 //check if a number is even or odd
-let a=3;
-if(a%2 === 0){
+let a = 3;
+if (a % 2 === 0) {
     console.log(`${a} is an even number`);
-}else{
+} else {
     console.log(`${a} is an odd number`)
 }
 
@@ -16,12 +16,40 @@ let hasNationaId = false;
 let hasSchoolId = false;
 let hasBag = true;
 
-if(hasNationaId && hasSchoolId && !hasBag){
+if (hasNationaId && hasSchoolId && !hasBag) {
     console.log("Enter the gate");
-}else if(hasBag && hasNationaId && hasSchoolId){
+} else if (hasBag && hasNationaId && hasSchoolId) {
     console.log("Open the bag");
-}else if(hasNationaId && !hasSchoolId){
+} else if (hasNationaId && !hasSchoolId) {
     console.log("Talk to Kife");
-}else{
+} else {
     console.log("Rudi nyumbani")
+}
+//switch
+let day = 7;
+switch (day) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Enter a valid day");
+        break;
 }
