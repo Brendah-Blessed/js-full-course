6a8efc71ec0cc51f3a35318c3cc6119930b664db
Brendah-Blessed <wawerubrendah63@gmail.com>
2024-07-01 14:16:48 +0300

    loops and functions

---
+++ Loops/index.js
@@ -0,0 +1,66 @@
+/**
+ * for loop
+ * while loop
+ * do while loop
+ * foreach 
+ * for in
+ * for for
+ */
+
+//for loop
+//for(initilizer; condition; update statement){
+//   code comes here
+//}
+
+for(let i = 0; i <= 10; i++){
+    //code here
+    //console.log(i);
+}
+
+
+//create a simple for loop to calculate the sum of the first 50 numbers
+
+let sum = 0
+
+for( let i =0; i <=50; i++){
+     sum = sum +i;    
+}
+//console.log(sum)
+
+//display all the even number between 0-100
+for(let i =0; i<= 100; i++){
+    if(i%2 === 0){
+       // console.log(i)
+    }
+}
+
+/**create  a simple program that calculates the sum
+ * of odd numbers between 0-50
+ */
+for(let i =0; i<= 50; i++){
+    if(i%2 === 0){
+       // console.log(i)
+       sum = sum + i;
+    }
+}
+console.log(sum);
+
+let j=0;
+
+/*while(j <= 10){
+    console.log(j);
+    j++;
+}
+
+do{
+    console.log(j);
+    j++;
+}while(j <= 10)
+*/
+//nested loops
+
+ 
+
+
+
+
---
+++ functions/index.js
@@ -0,0 +1,14 @@
+function multiply(a,b){
+    return a * b;
+}
+
+console.log(multiply(3,4));
+
+//second way 
+let firstNum = 10;
+let secondNum =20;
+
+console.log(multiply(firstNum, secondNum))
+
+let result = multiply(4,5);
+console.log(result);
